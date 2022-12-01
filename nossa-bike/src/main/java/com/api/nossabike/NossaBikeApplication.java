b70982f1b909674be500dbd5c16477421742d09a
CristionAssis <57966693+NNoiit@users.noreply.github.com>
2022-12-01 13:36:26 -0300

    teste teste
    teste de captura do sonar

--- nossa-bike/src/main/java/com/api/nossabike/NossaBikeApplication.java
+++ nossa-bike/src/main/java/com/api/nossabike/NossaBikeApplication.java
@@ -8,7 +8,6 @@ public class NossaBikeApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(NossaBikeApplication.class, args);
-    //teste de deploy para sonar
   }
 
 }
