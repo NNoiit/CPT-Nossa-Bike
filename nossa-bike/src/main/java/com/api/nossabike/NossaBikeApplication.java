30a5f38a879392dfafc65f6a125cd26abf3596a3
Cristion A.Assis <57966693+NNoiit@users.noreply.github.com>
2022-12-01 03:32:31 -0300

    Checkstyle e Juniti iniciados

---
+++ nossa-bike/src/main/java/com/api/nossabike/NossaBikeApplication.java
@@ -0,0 +1,13 @@
+package com.api.nossabike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossaBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossaBikeApplication.class, args);
+	}
+
+}
---
+++ nossa-bike/src/test/java/com/api/nossabike/NossaBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.nossabike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossaBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
